d28f530b3bbea1f79e0486580d828ae0170a9980
D-Jy-lab <duan@d-jy.net>
2023-04-27 22:13:26 +0800

    fix bug

--- bing.py
+++ bing.py
@@ -20,7 +20,7 @@ HOST = "0.0.0.0"
 PORT = 8007
 
 # 手动配置时按需要修改cookie文件路径，将./cookie.json修改为你自己的文件位置（一般不用修改）
-chatbot = Chatbot(cookie_path='./cookies.json')
+chatbot = Chatbot(cookie_path='./cookie.json')
 flag = False
 
 
